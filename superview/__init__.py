fc82c4f636a6a7750d4923d9877141741fb16f38
Andrey Antukh <niwi@niwi.be>
2012-08-08 23:13:01 +0200

    Add as_json templatetag.

--- superview/__init__.py
+++ superview/__init__.py
@@ -1,1 +1,1 @@
-__version__ = (0, 3, 6, "final", 0)
+__version__ = (0, 3, 7, "final", 0)
--- superview/templatetags/superview.py
+++ superview/templatetags/superview.py
@@ -2,6 +2,9 @@
 
 from django import template
 from django.utils.safestring import mark_safe
+from ..utils import LazyEncoder
+
+import types, json
 
 register = template.Library()
 
@@ -15,3 +18,11 @@ def call_view_method(context, method_name, *args, **kwargs):
     del view.current_context
 
     return method_response
+
+
+@register.simple_tag(name="as_json")
+def as_json(data):
+    if isinstance(data, types.GeneratorType):
+        data = tuple(data)
+    json_data = json.dumps(data, cls=LazyEncoder)
+    return mark_safe(json_data)
