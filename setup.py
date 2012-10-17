2626cc88e83e7d8c588e23dc85a2bdcf0996311c
Andrey Antukh <niwi@niwi.be>
2012-10-18 00:19:08 +0200

    Increment version and add some useful methods.

--- setup.py
+++ setup.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import superview
 
 setup(
     name = 'django-superview',
-    version = ":versiontools:superview:",
+    version = "0.4",
     description = "Generic View class with extra funionality for Django",
     long_description = "",
     keywords = 'django, view',
     author = 'Jesús Espino García & Andrei Antoukh',
     author_email = 'jespinog@gmail.com, niwi@niwi.be',
-    url = 'https://github.com/jespino/django-superview',
+    url = 'https://github.com/kaleidos/django-superview',
     license = 'BSD',
     include_package_data = True,
     packages = find_packages(),
     install_requires=[
-        'distribute',
-    ],
-    setup_requires = [
-        'versiontools >= 1.8',
+        'pytz',
     ],
     classifiers = [
         "Programming Language :: Python",
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
+        "Development Status :: 5 - Production/Stable",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Django',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ]
--- superview/utils.py
+++ superview/utils.py
@@ -3,13 +3,9 @@
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.functional import Promise
 from django.utils.encoding import force_unicode
-
-try:
-    from django.utils.timezone import localtime
-except ImportError:
-    localtime = lambda x: x
-
+from django.utils import timezone
 import datetime
+
 
 class LazyEncoder(DjangoJSONEncoder):
     """
@@ -21,6 +17,44 @@ class LazyEncoder(DjangoJSONEncoder):
         if isinstance(obj, Promise):
             return force_unicode(obj)
         elif isinstance(obj, datetime.datetime):
-            obj = localtime(obj)
+            obj = timezone.localtime(obj)
         return super(LazyEncoder, self).default(obj)
 
+
+def datetime_to_ecma262(value):
+    """
+    Convert a convencional datetime object
+    to ecma-262 javascript/json format.
+    """
+    r = value.isoformat()
+    if value.microsecond:
+        r = r[:23] + r[26:]
+    if r.endswith('+00:00'):
+        r = r[:-6] + 'Z'
+    return r
+
+
+def ecma262_to_datetimne(value):
+    """
+    Convert ecma-262 javascript format to
+    correct python datetime format.
+
+    This always return a timezone aware datetimes
+    with utc as timezone.
+    """
+
+    spec = "%Y-%m-%dT%H:%M:%S.%f"
+    is_utc = False
+
+    if value.endswith("Z"):
+        is_utc = True
+        value = value[:-1]
+
+    dt = strptime(value, spec)
+    if is_utc:
+        return timezone.make_aware(dt, timezone.utc)
+
+    dt = timezone.make_aware(dt, timezone.get_current_timezone())
+    dt = timezone.astimezone(timezone.utc)
+    dt = timezone.utc.normalize(dt)
+    return dt
